612ba52b60d13354c6ce8d020f0250f8d9578bd6
AxelDp04 <axelp7223@gmail.com>
2026-07-02 15:13:12 -0400

    Mi primer Hola Mundo en C# correcto

--- Metodologia_agil/obj/Debug/net8.0/Metodologia_agil.AssemblyInfo.cs
+++ Metodologia_agil/obj/Debug/net8.0/Metodologia_agil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Metodologia_agil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57d2563129113fc3a5e01e8029a4548330039f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Metodologia_agil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Metodologia_agil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
